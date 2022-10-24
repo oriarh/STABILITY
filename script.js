4658a3c09fbd548c587097a913f52cf381c8d5a3
oriarh <111208485+oriarh@users.noreply.github.com>
2022-10-24 14:51:12 -0700

    adding files to the repo

---
+++ script.js
@@ -0,0 +1,8 @@
+var inputName = document.getElementById("name");
+var inputEmail = document.getElementById("email");
+var button = document.getElementById("submit");
+
+button.addEventListener("click", function (event) {
+    console.log(inputName.value);
+
+});
