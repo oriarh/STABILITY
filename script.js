973b05705f8afbcf4550c32109cb39728ad46bf5
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-10-25 15:48:52 -0700

    added new js file with graph function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var inputName = document.getElementById("name");
 var inputEmail = document.getElementById("email");
 var button = document.getElementById("submit");
+var chartBtn = document.getElementById("chartBtn");
+
+
+
 
 button.addEventListener("click", function (event) {
     console.log(inputName.value);
 
 });
+
+
+chartBtn.addEventListener("click", function handleclick() {
+    var lineChart = document.getElementById("myChart");
+    if (lineChart.style.display === "none") {
+        lineChart.style.display = "block";
+    } else {
+        lineChart.style.display = "none";
+    }
+})
