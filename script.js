826f08ca544300dac0b4b1f3ba1c6a1778f22bbb
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-10-25 17:54:53 -0700

    added graph js

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ button.addEventListener("click", function (event) {
 
 });
 
+var predictedMood;
+var food = $(inputFood);
+var fitness = $(inputFitness);
+var sleep = $(inputSleep);
+var Mood = $(inputMood);
+let coefFood = [0.25, 0.75];
+let coefFitness = [0.25, 0.75];
+let coatSleep = [0.25, 0.75];
+let coefMood = [0.25, 0.5, 0.75];
+let resultFood = food * coefFood[placeHolder];
+let resultFitness = fitness * coeffitness[placeHolder];
+let resultsleep = sleep * coefDleep[placeHolder];
+let resultMood = Mood * coefMood[placeHolder];
+let predictedValue = resultFood + resultFitness + resultMood + resultSleep;
+if (predictedValue < 0.35) {
+    predictedMood = 'Bad';
+} else if (predictedValue < 0.55 && predictedValue >= 0.35) {
+    predictedMood = 'Decent';
+} else if (predictedValue > 0.55) {
+    predictedMood = 'Good ';
+}
+
 
 chartBtn.addEventListener("click", function handleclick() {
     var lineChart = document.getElementById("myChart");
